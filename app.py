2eac64923fdf12d7a910ca13624638b1fa35b801
Gleb Matveev <glebmatveev34@gmail.com>
2025-03-11 18:45:33 +1000

    2

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
-#
-#
-# if __name__ == '__main__':
-#     app.run()
 from flask import Flask
 from structures.views import index
 
 app = Flask(__name__)
-
-if __name__ == 'main':
-  app.run(debug=True)
-
+app.register_blueprint(index)
--- structures/views.py
+++ structures/views.py
@@ -1,5 +1,15 @@
+from flask import Blueprint, render_template
 from app import app
-
+from structures.models import get_all_buildings
 @app.route('/')
 def index():
-    return 'Hello, world!'
+
+    [buildings_head, buildings_body] = get_all_buildings()
+
+    html = render_template(
+        'index.html',
+        buildings_head=buildings_head,
+        buildings_body=buildings_body
+    )
+
+    return html
